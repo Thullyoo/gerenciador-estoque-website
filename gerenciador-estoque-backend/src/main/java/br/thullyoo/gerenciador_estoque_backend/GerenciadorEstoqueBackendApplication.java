226d3ac04831b51ef1b5e75e03349144f9a1f06b
Thullyoo <thullyofilho@gmail.com>
2025-03-28 11:57:47 -0300

    Initial commit

---
+++ gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/GerenciadorEstoqueBackendApplication.java
@@ -0,0 +1,13 @@
+package br.thullyoo.gerenciador_estoque_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorEstoqueBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorEstoqueBackendApplication.class, args);
+	}
+
+}
---
+++ gerenciador-estoque-backend/src/test/java/br/thullyoo/gerenciador_estoque_backend/GerenciadorEstoqueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyoo.gerenciador_estoque_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEstoqueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
