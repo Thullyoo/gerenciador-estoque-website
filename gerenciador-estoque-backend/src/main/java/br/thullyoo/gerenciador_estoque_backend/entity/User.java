48dfb825710549659854a7f18005fb61aecd4014
Thullyoo <thullyofilho@gmail.com>
2025-04-03 13:51:03 -0300

    feat: add User Model

---
+++ gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/entity/User.java
@@ -0,0 +1,80 @@
+package br.thullyoo.gerenciador_estoque_backend.entity;
+
+import br.thullyoo.gerenciador_estoque_backend.entity.enums.AuthProvider;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
+    private String pictureUrl;
+
+    @Enumerated(EnumType.STRING)
+    private AuthProvider authProvider;
+
+    public User() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public AuthProvider getAuthProvider() {
+        return authProvider;
+    }
+
+    public void setAuthProvider(AuthProvider authProvider) {
+        this.authProvider = authProvider;
+    }
+}
+
+
