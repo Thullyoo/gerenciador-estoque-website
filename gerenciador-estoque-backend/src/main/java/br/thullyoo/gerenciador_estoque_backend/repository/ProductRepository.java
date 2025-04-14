95fd038698a58b9acef393a802af90126922a2f7
Thullyoo <thullyofilho@gmail.com>
2025-04-14 20:14:45 -0300

    feat: add method to get owner

--- gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/repository/ProductRepository.java
+++ gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/repository/ProductRepository.java
@@ -1,7 +1,12 @@
 package br.thullyoo.gerenciador_estoque_backend.repository;
 
 import br.thullyoo.gerenciador_estoque_backend.entity.Product;
+import br.thullyoo.gerenciador_estoque_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<List<Product>> findByOwner(User user);
 }
