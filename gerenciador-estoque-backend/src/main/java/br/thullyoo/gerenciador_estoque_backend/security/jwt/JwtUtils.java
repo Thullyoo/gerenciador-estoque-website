b460beade858ca42635e6a081ce1ab7666408a7c
Thullyoo <thullyofilho@gmail.com>
2025-05-02 12:24:57 -0300

    feat: add method to get user by token

--- gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/security/jwt/JwtUtils.java
+++ gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/security/jwt/JwtUtils.java
@@ -1,11 +1,14 @@
 package br.thullyoo.gerenciador_estoque_backend.security.jwt;
 
+import br.thullyoo.gerenciador_estoque_backend.entity.User;
+import br.thullyoo.gerenciador_estoque_backend.repository.UserRepository;
 import br.thullyoo.gerenciador_estoque_backend.security.UserDetailsImpl;
 import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import java.security.Key;
 import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -13,6 +16,7 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.Date;
+import java.util.Optional;
 
 @Component
 public class JwtUtils {
@@ -26,11 +30,14 @@ public class JwtUtils {
     @Value("${jwt.public.key}")
     private String jwtPublicKey;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public String generateTokenByUserDetails(UserDetailsImpl user) {
         return Jwts.builder()
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + jwtTimeExpiration * 1000L))
+                .setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000L))
                 .signWith(getPrivateKey(), SignatureAlgorithm.RS256)
                 .compact();
     }
@@ -52,7 +59,7 @@ public class JwtUtils {
         }
     }
 
-    private RSAPublicKey getPublicKey() {
+    public RSAPublicKey getPublicKey() {
         try {
             String publicKeyPEM = jwtPublicKey
                     .replace("-----BEGIN PUBLIC KEY-----", "")
@@ -82,12 +89,19 @@ public class JwtUtils {
         }
     }
 
-    public String getUsernameToken(String jwt) {
-        return Jwts.parser()
-                .verifyWith(getPublicKey())
-                .build()
-                .parseClaimsJws(jwt)
-                .getBody()
-                .getSubject();
+    public User getUserByToken() {
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String email;
+
+        if (principal instanceof UserDetails) {
+            email = ((UserDetails) principal).getUsername();
+        } else {
+            email = principal.toString();
+        }
+
+        Optional<User>  user = userRepository.findByEmail(email);
+        return user.get();
     }
 }
