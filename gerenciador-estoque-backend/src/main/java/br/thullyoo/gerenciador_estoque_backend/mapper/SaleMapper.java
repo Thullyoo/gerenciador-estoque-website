18ea7a55a7c707cde5b8c17a80bbfd23fc4dd199
Thullyoo <thullyofilho@gmail.com>
2025-05-07 19:07:41 -0300

    feat: add method to map Sale to SaleResponse

--- gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/mapper/SaleMapper.java
+++ gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/mapper/SaleMapper.java
@@ -1,8 +1,13 @@
 package br.thullyoo.gerenciador_estoque_backend.mapper;
 
+import br.thullyoo.gerenciador_estoque_backend.dto.response.SaleItemResponse;
+import br.thullyoo.gerenciador_estoque_backend.dto.response.SaleResponse;
 import br.thullyoo.gerenciador_estoque_backend.entity.Product;
 import br.thullyoo.gerenciador_estoque_backend.entity.Sale;
 import br.thullyoo.gerenciador_estoque_backend.entity.SaleItem;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class SaleMapper {
 
@@ -13,6 +18,15 @@ public class SaleMapper {
         saleItem.setProduct(product);
         saleItem.setSale(sale);
         return saleItem;
+    }
+
+    public static SaleResponse toSaleResponse(Sale sale){
+        List<SaleItemResponse> itemResponses = sale.getItems().stream().map(SaleMapper::toSaleItemResponse).toList();
+        return new SaleResponse(sale.getDate() , sale.getTotalAmount(), itemResponses);
+    }
+
+    public static SaleItemResponse toSaleItemResponse(SaleItem saleItem){
+        return new SaleItemResponse(saleItem.getProduct().getName(), saleItem.getProduct().getCode(), saleItem.getUnitPrice(), saleItem.getQuantity());
     }
 
 }
