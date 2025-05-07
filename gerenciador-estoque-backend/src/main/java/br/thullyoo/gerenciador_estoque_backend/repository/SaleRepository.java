bd9b431760fdb9d82ff4d6ff6b54f8e03dc41b61
Thullyoo <thullyofilho@gmail.com>
2025-05-07 19:07:14 -0300

    feat: add method to get Seller By User

--- gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/repository/SaleRepository.java
+++ gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/repository/SaleRepository.java
@@ -1,7 +1,11 @@
 package br.thullyoo.gerenciador_estoque_backend.repository;
 
 import br.thullyoo.gerenciador_estoque_backend.entity.Sale;
+import br.thullyoo.gerenciador_estoque_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+    List<Sale> findBySeller(User user);
 }
