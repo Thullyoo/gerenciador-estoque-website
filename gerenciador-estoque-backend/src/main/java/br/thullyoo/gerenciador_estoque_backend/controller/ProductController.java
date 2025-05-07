4cb88dbf8fb350b0674dd425689a44eb14889dfa
Thullyoo <thullyofilho@gmail.com>
2025-05-07 18:48:19 -0300

    feat: add endpoint to get products by user

--- gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/controller/ProductController.java
+++ gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/controller/ProductController.java
@@ -1,14 +1,14 @@
 package br.thullyoo.gerenciador_estoque_backend.controller;
 
 import br.thullyoo.gerenciador_estoque_backend.dto.request.ProductRequest;
+import br.thullyoo.gerenciador_estoque_backend.dto.response.ProductResponse;
 import br.thullyoo.gerenciador_estoque_backend.entity.Product;
 import br.thullyoo.gerenciador_estoque_backend.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -23,5 +23,10 @@ public class ProductController {
     @PostMapping("/register")
     public ResponseEntity<Product> registerProduct(@RequestBody ProductRequest productRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.registerProduct(productRequest));
+    }
+
+    @GetMapping("/myproducts")
+    public ResponseEntity<List<ProductResponse>> listProductsByUser(){
+        return ResponseEntity.status(HttpStatus.OK).body(productService.listProductsByUser());
     }
 }
