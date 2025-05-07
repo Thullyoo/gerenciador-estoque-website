4745118706adb217d78b132654c48914b95a56e3
Thullyoo <thullyofilho@gmail.com>
2025-05-07 19:08:08 -0300

    feat: add endpoint to get Sales by User

--- gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/controller/SaleController.java
+++ gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/controller/SaleController.java
@@ -1,16 +1,15 @@
 package br.thullyoo.gerenciador_estoque_backend.controller;
 
 import br.thullyoo.gerenciador_estoque_backend.dto.request.SaleItemRequest;
+import br.thullyoo.gerenciador_estoque_backend.dto.response.SaleResponse;
 import br.thullyoo.gerenciador_estoque_backend.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/sale")
@@ -25,6 +24,9 @@ public class SaleController {
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 
-
+    @GetMapping("/mysales")
+    public ResponseEntity<List<SaleResponse>> listSalesByUser(){
+        return ResponseEntity.status(HttpStatus.OK).body(saleService.listSalesByUser());
+    }
 
 }
