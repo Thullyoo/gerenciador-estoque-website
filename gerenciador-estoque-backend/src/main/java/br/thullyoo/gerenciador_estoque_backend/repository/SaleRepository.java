f5fc72c2500022f34b2e5d6014270c1586d84576
Thullyoo <thullyofilho@gmail.com>
2025-05-08 16:45:27 -0300

    feat: add method to get Sales between a specify date

--- gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/repository/SaleRepository.java
+++ gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/repository/SaleRepository.java
@@ -4,8 +4,12 @@ import br.thullyoo.gerenciador_estoque_backend.entity.Sale;
 import br.thullyoo.gerenciador_estoque_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
     List<Sale> findBySeller(User user);
+
+    List<Sale> findBySellerAndDateBetween(User seller, LocalDateTime start, LocalDateTime end);
+
 }
