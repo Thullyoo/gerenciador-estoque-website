9670eb5cef11257bb23910ce828d296a73066a61
Thullyoo <thullyofilho@gmail.com>
2025-05-08 16:46:03 -0300

    feat: add toString to saleItemResponse

--- gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/dto/response/SaleItemResponse.java
+++ gerenciador-estoque-backend/src/main/java/br/thullyoo/gerenciador_estoque_backend/dto/response/SaleItemResponse.java
@@ -1,4 +1,12 @@
 package br.thullyoo.gerenciador_estoque_backend.dto.response;
 
 public record SaleItemResponse(String name, long code, double unitPrice, int quantity) {
+    @Override
+    public String toString() {
+        return
+                "Nome: " + name + "\n" +
+                "Codígo: " + code + "\n" +
+                "Preço da unidade: " + unitPrice + "\n" +
+                "Quantidade: " + quantity + "x\n";
+    }
 }
